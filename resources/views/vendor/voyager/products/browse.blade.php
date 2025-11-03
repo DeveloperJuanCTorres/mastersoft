b18def923874c93e10c88e78ec8da4f6d9d890b5
Juan Carlos Torres del Castillo <jctorresdelcastillo@gmail.com>
2025-11-03 09:32:25 -0500

    css

--- resources/views/vendor/voyager/products/browse.blade.php
+++ resources/views/vendor/voyager/products/browse.blade.php
@@ -356,7 +356,7 @@
 
 @section('javascript')
 
-    <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
+    <!-- <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" /> -->
     <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
     <!-- DataTables -->
     @if(!$dataType->server_side && config('dashboard.data_tables.responsive'))
